51b4c0cc8bbfaba9b94b428c0ea419326af8b764
khansanoor <82741826+khansanoor@users.noreply.github.com>
2025-08-12 23:07:50 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,11 @@
-import pandas
+import streamlit as st
+import pandas as pd
+import duckdb
+import os
+
+# --- Configuration ---
+st.set_page_config(
+    page_title="Huda Beauty SQL Challenge",
+    layout="wide",
+    #initial_sidebar_state="expanded"
+)
